618b1f4d60fd320023eb040a4ab051aa60593202
Domenique <domogenesis77@gmail.com>
2023-07-26 16:24:38 -0500

    pass

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, possible_anagrams):
+        matches = []
+        for anagram in possible_anagrams:
+            if sorted(anagram) == self.sorted_word:
+                matches.append(anagram)
+        return matches
